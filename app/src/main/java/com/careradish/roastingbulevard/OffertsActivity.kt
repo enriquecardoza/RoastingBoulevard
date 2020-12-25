e0c2338482f9b4e90298c0348d7f8b1920594e90
Enrique <enrique.cardoza@hotmail.com>
2020-12-25 13:19:58 +0100

    Strings y cambio nombre

---
+++ app/src/main/java/com/careradish/roastingbulevard/OffertsActivity.kt
@@ -0,0 +1,11 @@
+package com.careradish.roastingbulevard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OffertsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offerts)
+    }
+}
