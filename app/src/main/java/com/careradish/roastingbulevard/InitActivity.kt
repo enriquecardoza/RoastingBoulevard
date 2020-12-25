b02cefa4730bb940cc885bf835fa6ded7bc9a8a2
Enrique <enrique.cardoza@hotmail.com>
2020-12-25 18:19:10 +0100

    Activity init

---
+++ app/src/main/java/com/careradish/roastingbulevard/InitActivity.kt
@@ -0,0 +1,11 @@
+package com.careradish.roastingbulevard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_init)
+    }
+}
