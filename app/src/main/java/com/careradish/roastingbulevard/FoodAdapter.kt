254a2c936ad49ebae93c2a2a6111b221f99ed7a1
enriquecardoza <enrique.cardoza@hotmail.com>
2021-01-07 13:00:46 +0100

    Firebase test

--- app/src/main/java/com/careradish/roastingbulevard/FoodAdapter.kt
+++ app/src/main/java/com/careradish/roastingbulevard/FoodAdapter.kt
@@ -1,23 +1,48 @@
 package com.careradish.roastingbulevard
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class FoodAdapter(var comidas: List<Food>) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+ //  database.child("users").child(userId).setValue(user)
+
+
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         lateinit var name: TextView;
         lateinit var image: ImageView;
-
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Foods/"+"Migas")
+        var ss:String="test"
         init {
             name = itemView.findViewById(R.id.textView)
             image = itemView.findViewById(R.id.imageView)
+            itemView.setOnClickListener {
 
+
+                myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        ss=dataSnapshot.getValue().toString()
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // ...
+                    }
+                })
+                Toast.makeText(name.context, ss, Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
@@ -27,11 +52,11 @@ class FoodAdapter(var comidas: List<Food>) : RecyclerView.Adapter<FoodAdapter.Vi
         val view: View? = inflater?.inflate(R.layout.fooditemcard, parent, false)
         return FoodAdapter.ViewHolder(view!!)
     }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val food: Food = comidas[position]
+        val food = comidas[position]
         holder.name.text = food.nombre
         holder.image.setImageResource(food.image)
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/careradish/roastingbulevard/MainActivity.kt
+++ app/src/main/java/com/careradish/roastingbulevard/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.careradish.roastingbulevard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,16 +27,40 @@ class MainActivity : AppCompatActivity() {
         miAdapter = FoodAdapter(DatosClientes())
 
         miRecicler.adapter = miAdapter;
+
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+       // myRef.setValue("Hello, World!")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                Toast.makeText(applicationContext, "Value is: $value", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, "Failed to read value.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 
 
 
     private fun DatosClientes(): List<Food> {
         val Lista: MutableList<Food> = ArrayList()
-        Lista.add(Food(0,"Rey",R.mipmap.shrek))
-        Lista.add(Food(0,"Gil",R.mipmap.shrek))
-        Lista.add(Food(0,"Alonso",R.mipmap.shrek))
+        Lista.add(Food(0, "Rey", R.mipmap.shrek))
+        Lista.add(Food(0, "Gil", R.mipmap.shrek))
+        Lista.add(Food(0, "Alonso", R.mipmap.shrek))
         return Lista
+    }
+
+    public fun ShowToast(texto: String) {
+        Toast.makeText(applicationContext, texto, Toast.LENGTH_SHORT).show()
     }
 }
 
