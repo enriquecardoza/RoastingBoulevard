b3fa15777e9db41ce3f03139c3606831c182a9a5
enriquecardoza <enrique.cardoza@hotmail.com>
2021-01-07 12:16:01 +0100

    Card view

--- app/src/main/java/com/careradish/roastingbulevard/FoodAdapter.kt
+++ app/src/main/java/com/careradish/roastingbulevard/FoodAdapter.kt
@@ -7,35 +7,34 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FoodAdapter(var comidas:List<Food>) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+class FoodAdapter(var comidas: List<Food>) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
 
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var name: TextView;
+        lateinit var image: ImageView;
 
-    class  ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        lateinit var name :TextView;
-        lateinit var image :ImageView;
         init {
-            name=itemView.findViewById(R.id.textView)
-            image=itemView.findViewById(R.id.imageView)
+            name = itemView.findViewById(R.id.textView)
+            image = itemView.findViewById(R.id.imageView)
 
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater: LayoutInflater? =LayoutInflater.from(parent.context)
-        val view: View? =inflater?.inflate(R.layout.fooditemcard,parent,false)
+        val inflater: LayoutInflater? = LayoutInflater.from(parent.context)
+        val view: View? = inflater?.inflate(R.layout.fooditemcard, parent, false)
         return FoodAdapter.ViewHolder(view!!)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-        val food:Food=comidas[position]
-        holder.name.text=food.nombre
+        val food: Food = comidas[position]
+        holder.name.text = food.nombre
         holder.image.setImageResource(food.image)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return comidas.size
     }
 }
--- app/src/main/java/com/careradish/roastingbulevard/MainActivity.kt
+++ app/src/main/java/com/careradish/roastingbulevard/MainActivity.kt
@@ -2,12 +2,37 @@ package com.careradish.roastingbulevard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var miRecicler: RecyclerView
+    private lateinit var miAdapter: RecyclerView.Adapter<*>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        miRecicler=recliclador
+        miRecicler.setHasFixedSize(true)
+       // miRecicler.layoutManager = GridLayoutManager(this,2);
 
+        miAdapter = FoodAdapter(DatosClientes())
+
+        miRecicler.adapter = miAdapter;
+    }
+
+
+
+    private fun DatosClientes(): List<Food> {
+        val Lista: MutableList<Food> = ArrayList()
+        Lista.add(Food(0,"Rey",R.mipmap.shrek))
+        Lista.add(Food(0,"Gil",R.mipmap.shrek))
+        Lista.add(Food(0,"Alonso",R.mipmap.shrek))
+        return Lista
     }
 }
+
